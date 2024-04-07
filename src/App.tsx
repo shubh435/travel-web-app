2ac4f6cc072678c8a8495337a90708d9f2afb657
shubham sarode <shubham.sarode@extwebtech.in>
2024-04-07 13:43:18 +0530

    deploying for deeplinking

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,26 @@ import Services from "./components/Services";
 import GellerySection from "./components/GellerySection";
 import Blogs from "./components/Blogs";
 import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Setting from "./components/Setting";
 
 function App() {
   return (
-    <>
-      <Header />
-      <MainContainer />
-      <SectionForm />
-      <AboutUs />
-      <Services />
-      <GellerySection />
-      <Blogs />
-      <Footer />
-    </>
+
+    <BrowserRouter >
+      <Routes>
+        <Route path="/" element={<div>
+          <Header />
+          <MainContainer />
+          <SectionForm />
+          <AboutUs />
+          <Services />
+          <GellerySection />
+          <Blogs />
+          <Footer /> </div>} />
+          <Route path="/settings"Component={Setting} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Setting.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Setting extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Setting</h1>
+      </div>
+    )
+  }
+}
+
+export default Setting
